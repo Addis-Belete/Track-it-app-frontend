c4c9e254a3751e47bc976a40717c755a02ecdf5f
Addis Belete <addis.bel@gmail.com>
2021-08-12 10:46:54 +0300

    Add a simplified login component

---
+++ src/component/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Login = () => (
+  <div>
+    <input type="text" placeholder="Enter your name" />
+    <button type="button">Login</button>
+  </div>
+);
+export default Login;

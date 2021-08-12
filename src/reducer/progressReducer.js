05d0f7af574ea0915902ca984f70f4b0653b795e
Addis Belete <addis.bel@gmail.com>
2021-08-12 20:34:00 +0300

    Add progressReducer

--- src/reducer/progressReducer.js
+++ src/reducer/progressReducer.js
@@ -1,6 +1,37 @@
+import { FETCH_PROGRESS_FAILURE, FETCH_PROGRESS_REQUEST, FETCH_PROGRESS_SUCCESS } from '../store/type';
+
 const initialState = {
   isLoading: false,
   progress: [{ id: 1, result: 20 }],
   error: '',
 
 };
+
+const ProgressReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_PROGRESS_REQUEST:
+      return {
+        isLoading: true,
+        progress: [],
+        error: '',
+
+      };
+    case FETCH_PROGRESS_SUCCESS:
+      return {
+        isLoading: false,
+        progress: action.payload,
+        error: '',
+
+      };
+    case FETCH_PROGRESS_FAILURE:
+      return {
+        isLoading: false,
+        progress: [],
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default ProgressReducer;

ab55cd53c2103f5d908c1fae145a245fbc2c97ef
Addis Belete <addis.bel@gmail.com>
2021-08-13 09:14:02 +0300

    Add clickHandler method to dispatch based on id

--- src/container/Panel.js
+++ src/container/Panel.js
@@ -1,8 +1,26 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import { connect } from 'react-redux';
+import { connect, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import fetchProgress from '../action/Progress';
 
-const Panel = () => (
-  <div>Responsible to dispaly each category</div>
+const Panel = () => {
+  const dispatch = useDispatch;
+  const clickHandler = (id) => {
+    dispatch(fetchProgress(id));
+  };
 
-);
+  return (
+    <div>
+      <Link>Left bicep</Link>
+      <Link>Right bicep</Link>
+      <Link>Waist</Link>
+      <Link>Hips</Link>
+      <Link>Left thigh</Link>
+      <Link>Right thigh</Link>
+
+    </div>
+
+  );
+};
 export default connect()(Panel);

c7b8555d1ebaf1c7a3b11ee6ecb2dd9f4e42f594
Addis Belete <addis.bel@gmail.com>
2021-08-16 10:54:59 +0300

    Install bootStrap-button

--- src/component/Login.js
+++ src/component/Login.js
@@ -2,7 +2,8 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Form from 'react-bootstrap/Form'
+import Form from 'react-bootstrap/Form';
+
 const Login = () => {
   const [name, setName] = useState('');
   const validateForm = () => (name.length > 0);
@@ -10,7 +11,24 @@ const Login = () => {
     e.preventDefault();
   };
   return (
-    <Form >
+    <div className="login">
+      <Form onSubmit={hanldleSubmit}>
+        <Form.Group size="lg" controlId="name">
+          <Form.Label>User Name</Form.Label>
+          <Form.Control
+            autoFocus
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Group>
+        <Button block size="lg" type="submit" disabled={!validateForm()}>
+          Login
+        </Button>
+      </Form>
+
+    </div>
+
   );
 };
 export default Login;

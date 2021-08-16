e28e24b1d202306dd18a17842c5724a776e13962
Addis Belete <addis.bel@gmail.com>
2021-08-16 16:36:37 +0300

    Add font awesome icons

--- src/component/footer.js
+++ src/component/footer.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faPlusSquare, faChartLine, faChartPie, faSignOutAlt,
+} from '@fortawesome/free-solid-svg-icons';
 
 const Navigation = (path) => {
   if (path === '/') {
@@ -8,10 +12,32 @@ const Navigation = (path) => {
   return (
     <div className="footer">
 
-      <Link to="/addMeasurment">Add</Link>
-      <Link to="/track">Track</Link>
-      <Link to="/Progress">Progress</Link>
-      <Link to="/">Logout</Link>
+      <Link to="/addMeasurment">
+        <div>
+          <FontAwesomeIcon icon={faPlusSquare} />
+          <p>Add Measure</p>
+        </div>
+
+      </Link>
+      <Link to="/track">
+        <div>
+          <FontAwesomeIcon icon={faChartLine} />
+          <p>Track</p>
+
+        </div>
+      </Link>
+      <Link to="/Progress">
+        <div>
+          <FontAwesomeIcon icon={faChartPie} />
+          <p>Progress</p>
+        </div>
+      </Link>
+      <Link to="/">
+        <div>
+          <FontAwesomeIcon icon={faSignOutAlt} />
+          <p>Logout</p>
+        </div>
+      </Link>
     </div>
   );
 };

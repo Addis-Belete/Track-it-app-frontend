2d77d8e2ca45c9d14de6efea851399839c622ab9
Addis Belete <addis.bel@gmail.com>
2021-08-16 10:46:38 +0300

    Install react-bootstarp form

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,9 +1,16 @@
-import React from 'react';
-
-const Login = () => (
-  <div>
-    <input type="text" placeholder="Enter your name" />
-    <button type="button">Login</button>
-  </div>
-);
+/* eslint-disable consistent-return */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Form from 'react-bootstrap/Form'
+const Login = () => {
+  const [name, setName] = useState('');
+  const validateForm = () => (name.length > 0);
+  const hanldleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <Form >
+  );
+};
 export default Login;
--- src/component/footer.js
+++ src/component/footer.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Navigation = () => (
-  <div>
-    <Link to="/addMeasurment">Add</Link>
-    <Link to="/track">Track</Link>
-    <Link to="/Progress">Progress</Link>
-    <Link to="/">Logout</Link>
-  </div>
-);
+const Navigation = (path) => {
+  if (path === '/') {
+    return '';
+  }
+  return (
+    <div>
+
+      <Link to="/addMeasurment">Add</Link>
+      <Link to="/track">Track</Link>
+      <Link to="/Progress">Progress</Link>
+      <Link to="/">Logout</Link>
+    </div>
+  );
+};
 
 export default Navigation;

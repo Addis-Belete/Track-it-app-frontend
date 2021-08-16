24700a7fe666cb807270f86b1cd75ba71d49ca7a
Addis Belete <addis.bel@gmail.com>
2021-08-16 17:57:11 +0300

    Add style to panel component

--- src/container/Panel.js
+++ src/container/Panel.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchProgress } from '../action/Progress';
 import Navigation from '../component/footer';
+import panel from '../style/panel.module.css';
 
 const Panel = () => {
   const dispatch = useDispatch();
@@ -12,13 +13,14 @@ const Panel = () => {
 
   return (
     <div>
-
-      <Link onClick={clickHandler(1)} to="/progress/1"><div>Left bicep</div></Link>
-      <Link onClick={clickHandler(2)} to="/progress/2"><div>Right bicep</div></Link>
-      <Link onClick={clickHandler(3)} to="/progress/3"><div>Hips</div></Link>
-      <Link onClick={clickHandler(4)} to="/progress/4"><div>Waist</div></Link>
-      <Link onClick={clickHandler(5)} to="/progress/5"><div>Left thigh</div></Link>
-      <Link onClick={clickHandler(6)} to="/progress/6"><div>Right thigh</div></Link>
+      <div className={panel.panel}>
+        <Link onClick={clickHandler(1)} to="/progress/1"><div className={panel.cat}>Left bicep</div></Link>
+        <Link onClick={clickHandler(2)} to="/progress/2"><div className={panel.cat}>Right bicep</div></Link>
+        <Link onClick={clickHandler(3)} to="/progress/3"><div className={panel.cat}>Hips</div></Link>
+        <Link onClick={clickHandler(4)} to="/progress/4"><div className={panel.cat}>Waist</div></Link>
+        <Link onClick={clickHandler(5)} to="/progress/5"><div className={panel.cat}>Left thigh</div></Link>
+        <Link onClick={clickHandler(6)} to="/progress/6"><div className={panel.cat}>Right thigh</div></Link>
+      </div>
       <Navigation />
     </div>
 

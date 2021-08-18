358343e4d374f68f7466a7a218f3f74c646d2d86
Addis Belete <addis.bel@gmail.com>
2021-08-18 10:40:37 +0300

    Add snapshot test for Login component

---
+++ src/__tests__/component/Login.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { cleanup, render } from '@testing-library/react';
+
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Login from '../../component/Login';
+import store from '../../store/store';
+import '@testing-library/jest-dom';
+
+afterEach(cleanup);
+describe('addMeasurment', () => {
+  it('should render a Login page', () => {
+    const { asFragment } = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Login />
+        </BrowserRouter>
+
+      </Provider>,
+
+    );
+    expect(
+      asFragment(
+        <Provider store={store}>
+          <Login />
+        </Provider>,
+
+      ),
+    );
+  });
+});

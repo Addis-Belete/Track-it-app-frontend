bab343be3ac09ffa77a953ff9ebb79e075a0ce60
Addis Belete <addis.bel@gmail.com>
2021-08-18 10:34:33 +0300

    Add snapshot test for footer component

---
+++ src/__tests__/component/footer.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { cleanup, render } from '@testing-library/react';
+
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Footer from '../../component/footer';
+import store from '../../store/store';
+import '@testing-library/jest-dom';
+
+afterEach(cleanup);
+describe('addMeasurment', () => {
+  it('should render a links ', () => {
+    const { asFragment } = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Footer />
+        </BrowserRouter>
+
+      </Provider>,
+
+    );
+    expect(
+      asFragment(
+        <Provider store={store}>
+          <Footer />
+        </Provider>,
+
+      ),
+    );
+  });
+});
